d0aacc5908625f16b59d4066a3bdec83f0cda984
Noah Larsen <noahlarsen24@gmail.com>
2022-12-20 15:28:40 -0600

    updated javascript

--- app.js
+++ app.js
@@ -1,26 +1,67 @@
-const submitBtn = document.querySelector(".btn");
-const input = document.querySelector(".input");
-const slider = document.querySelector(".slider");
-const tipPercent = document.getElementById("tip-percent");
-const tip = document.getElementById("tip");
-const total = document.getElementById("bill");
-// event listener for tip slider
-slider.addEventListener("input", updateSlider);
-// show tip percentage on slider
-function updateSlider() {
-  tipPercent.textContent = `${slider.value}%`;
+const bill = document.querySelector(".bill");
+const tipBtns = document.querySelectorAll(".tip-percent");
+const custom = document.querySelector(".custom");
+const split = document.querySelector(".split");
+const total = document.querySelector(".total-bill");
+const tip = document.querySelector(".tip");
+const reset = document.querySelector(".btn");
+const invalid = document.querySelector(".invalid");
+
+function getAmount() {
+  tipBtns.forEach((tipBtn) => {
+    tipBtn.addEventListener("click", () => {
+      // GET TIP AMOUNTS
+      if (tipBtn.textContent === "5%") {
+        tip.textContent = `$${parseFloat(
+          (0.05 * bill.value) / split.value
+        ).toFixed(2)}`;
+      } else if (tipBtn.textContent === "10%") {
+        tip.textContent = `$${parseFloat(
+          (0.1 * bill.value) / split.value
+        ).toFixed(2)}`;
+      } else if (tipBtn.textContent === "15%") {
+        tip.textContent = `$${parseFloat(
+          (0.15 * bill.value) / split.value
+        ).toFixed(2)}`;
+      } else if (tipBtn.textContent === "25%") {
+        tip.textContent = `$${parseFloat(
+          (0.25 * bill.value) / split.value
+        ).toFixed(2)}`;
+      } else if (tipBtn.textContent === "50%") {
+        tip.textContent = `$${parseFloat(
+          (0.5 * bill.value) / split.value
+        ).toFixed(2)}`;
+      } else if (custom) {
+        tip.textContent = `$${parseFloat(
+          (custom.value * 0.01 * bill.value) / split.value
+        ).toFixed(2)}`;
+      }
+      // ADD INVALID MESSAGE
+      if (split.value === "" || split.value === "0") {
+        split.style.border = "solid 2.25px hsl(355, 50%, 54%)";
+        invalid.style.display = "inline-block";
+      }
+      calculateTotal();
+    });
+  });
 }
-// calculate tip and total on click of submit button
-submitBtn.addEventListener("click", submitBill);
+getAmount();
 
-function submitBill() {
-  // change bill and slider value to numbers
-  const bill = parseFloat(input.value);
-  const sliderValue = parseFloat(slider.value);
-
-  // formula to get and display tip dollar amount
-  tip.textContent = `$${(sliderValue / 100) * bill.toFixed(2)}`;
-
-  // formula to get and display tip + bill
-  total.textContent = `$${(bill + (sliderValue / 100) * bill).toFixed(2)}`;
+// GET TOTAL PER PERSON
+function calculateTotal() {
+  total.textContent = `$${parseFloat(bill.value / split.value).toFixed(2)}`;
 }
+
+// REMOVE INVALID MESSAGE
+split.addEventListener("focus", () => {
+  invalid.style.display = "none";
+  split.style.border = "none";
+});
+
+// RESET CALCULATOR
+reset.addEventListener("click", () => {
+  bill.value = "";
+  split.value = "";
+  tip.textContent = "$0.00";
+  total.textContent = "$0.00";
+});
